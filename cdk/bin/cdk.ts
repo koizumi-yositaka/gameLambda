314d9c513a3b8169aeed32e0c286ba86b57c339e
koizumi-yoshitaka <k.y>
2025-11-24 13:14:05 +0900

    OIDC追加

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,10 +2,12 @@
 import * as cdk from "aws-cdk-lib";
 import { LambdaStudyStack } from "../lib/lambda-stack";
 import "dotenv/config";
+import { GitHubActionsRoleStack } from "../lib/githubActions-role-stack";
 const app = new cdk.App();
 
 const PREFIX = "game-line";
 
+new GitHubActionsRoleStack(app, "GitHubActionsRoleStack");
 // LINE Channel Access TokenとChannel Secretは環境変数から取得
 // デプロイ時: LINE_CHANNEL_ACCESS_TOKEN=your_token LINE_CHANNEL_SECRET=your_secret cdk deploy
 // または、デプロイ後にAWSコンソールでLambda関数の環境変数を設定
---
+++ cdk/lib/githubActions-role-stack.ts
@@ -0,0 +1,67 @@
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+
+const GITHUB_USERNAME = "koizumi-yositaka";
+const REPOSITORY_NAME_LIST = ["gameLambda"];
+
+export class GitHubActionsRoleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // GitHubのOIDCプロバイダーを作成
+    const githubProvider = new iam.OpenIdConnectProvider(
+      this,
+      "GitHubProvider",
+      {
+        url: "https://token.actions.githubusercontent.com",
+        clientIds: ["sts.amazonaws.com"],
+      }
+    );
+
+    // github actions からの使用を許可したIAMロールを作成
+    const stringLike = REPOSITORY_NAME_LIST.map(
+      (x) => `repo:${GITHUB_USERNAME}/${x}:*`
+    );
+    console.log(stringLike);
+    const githubActionsRole = new iam.Role(this, "GitHubActionsRole", {
+      assumedBy: new iam.WebIdentityPrincipal(
+        githubProvider.openIdConnectProviderArn,
+        {
+          StringEquals: {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+          },
+          StringLike: {
+            "token.actions.githubusercontent.com:sub": stringLike,
+          },
+        }
+      ),
+      description: "Role for GitHub Actions to deploy CDK stack",
+    });
+
+    // CDKデプロイに必要な権限を付与
+    githubActionsRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "cloudformation:*",
+          "iam:*",
+          "logs:*",
+          "s3:*",
+          "ssm:*",
+          "ec2:*",
+          "cloudfront:*",
+          "ecr:*",
+          "route53:*",
+        ],
+        resources: ["*"],
+      })
+    );
+
+    // ロールのARNを出力
+    new cdk.CfnOutput(this, "RoleArn", {
+      value: githubActionsRole.roleArn,
+      description: "ARN of the GitHub Actions role",
+    });
+  }
+}
